adbdb8c3ef65976a18d0e7af138cb24b4160ca55
Richa Deo <rddricha@gmail.com>
2016-04-13 16:56:55 +0530

    Add : Dao classes

---
+++ nvliHarvester/src/main/java/in/gov/nvli/harvester/dao/HarRecordMetadataDcDao.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package in.gov.nvli.harvester.dao;
+
+import in.gov.nvli.harvester.beans.HarRecordMetadataDc;
+
+/**
+ *
+ * @author richa
+ */
+public interface HarRecordMetadataDcDao {
+  
+  public HarRecordMetadataDc save(HarRecordMetadataDc metadataDc);
+  
+}
---
+++ nvliHarvester/src/main/java/in/gov/nvli/harvester/daoImpl/HarRecordMetadataDcDaoImpl.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package in.gov.nvli.harvester.daoImpl;
+
+import in.gov.nvli.harvester.beans.HarRecordMetadataDc;
+import in.gov.nvli.harvester.dao.HarRecordMetadataDcDao;
+
+/**
+ *
+ * @author richa
+ */
+public class HarRecordMetadataDcDaoImpl implements HarRecordMetadataDcDao{
+
+  @Override
+  public HarRecordMetadataDc save(HarRecordMetadataDc metadataDc) {
+    
+    return metadataDc;
+  }
+  
+  
+  
+}
