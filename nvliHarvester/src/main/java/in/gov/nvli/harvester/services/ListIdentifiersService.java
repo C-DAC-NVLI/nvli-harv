8522fef06b711d8e40d8e93c914823162ae0cfb4
vootla <vootla@hcdc-pc22.pune.cdac.in>
2016-04-13 17:12:04 +0530

    update:code enhancement

---
+++ nvliHarvester/src/main/java/in/gov/nvli/harvester/services/ListIdentifiersService.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package in.gov.nvli.harvester.services;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import javax.xml.bind.JAXBException;
+
+/**
+ *
+ * @author richa
+ */
+public interface ListIdentifiersService {
+  
+  public void getListIdentifiers(String baseUrl) throws MalformedURLException, IOException, JAXBException;
+  
+}
