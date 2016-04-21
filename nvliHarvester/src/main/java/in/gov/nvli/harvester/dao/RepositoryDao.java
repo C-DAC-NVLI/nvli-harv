85a0b16710104c400ae58ac6847c76714494c87e
vootla <vootla@hcdc-pc22.pune.cdac.in>
2016-04-21 15:33:54 +0530

    update: code enhancement

--- nvliHarvester/src/main/java/in/gov/nvli/harvester/dao/RepositoryDao.java
+++ nvliHarvester/src/main/java/in/gov/nvli/harvester/dao/RepositoryDao.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -18,3 +19,4 @@ public interface RepositoryDao extends GenericDao<HarRepo, Integer>{
     public HarRepo getRepository(String baseURL);
     public HarRepo getRepository(int repositoryId);
 }
+
