c1c331d29a344b176e01de6cda6247e088cdeca3
Ankit Sharma <sankit@cdac.in>
2016-04-26 17:23:55 +0530

    updated code to implement incremental harvesting

--- nvliHarvester/src/main/java/in/gov/nvli/harvester/daoImpl/HarRecordDaoImpl.java
+++ nvliHarvester/src/main/java/in/gov/nvli/harvester/daoImpl/HarRecordDaoImpl.java
@@ -11,6 +11,7 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,20 +24,22 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @Repository
 @Transactional(readOnly = true)
-public class HarRecordDaoImpl implements HarRecordDao {
+public class HarRecordDaoImpl extends GenericDaoImpl<HarRecord, Long> implements HarRecordDao {
   
   private static final Logger LOGGER = LoggerFactory.getLogger(HarRecordDaoImpl.class);
 
+    public HarRecordDaoImpl() {
+        super(HarRecord.class);
+    }
 
-  @Autowired
-  private SessionFactory sessionFactory;
+  
 
   @Override
   @Transactional
   public void saveHarRecord(HarRecord record) {
     Session session = null;
     try {
-      session = sessionFactory.getCurrentSession();
+      session = currentSession();
       session.save(record);
 
     } catch (Exception e) {
@@ -49,7 +52,7 @@ public class HarRecordDaoImpl implements HarRecordDao {
   public void saveListHarRecord(List<HarRecord> records) {
     Session session = null;
     try {
-      session = sessionFactory.getCurrentSession();
+      session = currentSession();
       for (HarRecord record : records) {
         session.save(record);
       }
@@ -58,5 +61,36 @@ public class HarRecordDaoImpl implements HarRecordDao {
       LOGGER.error(e.getMessage(),e);
     }
   }
+  
+  @Override
+  @Transactional
+  public void saveOrUpdateListHarRecord(List<HarRecord> records) {
+    HarRecord temprecord = null;
+    try {
+      for (HarRecord record : records) {
+          temprecord = getHarRecord(record.getRecordIdentifier());
+          if(temprecord != null){
+              record.setRecordId(temprecord.getRecordId());
+              currentSession().merge(record);
+          }else{
+              createNew(record);
+          }    
+      }
+    } catch (Exception e) {
+      LOGGER.error(e.getMessage(),e);
+    }
+  }
+
+    @Override
+    public HarRecord getHarRecord(String recordIdentifier) {
+        try{
+            return (HarRecord) currentSession().createCriteria(HarRecord.class).add(Restrictions.eq("recordIdentifier", recordIdentifier)).uniqueResult();
+        }catch(Exception e){
+            LOGGER.error(e.getMessage(),e);
+            return null;
+        }
+    }
+  
+  
 
 }
