f690c948f5978ad842aa8397a24d293cb2bad0fb
Ankit Sharma <sankit@cdac.in>
2016-04-27 18:31:22 +0530

    code optimization and formatting

--- nvliHarvester/src/main/java/in/gov/nvli/harvester/daoImpl/IdentifyDaoImpl.java
+++ nvliHarvester/src/main/java/in/gov/nvli/harvester/daoImpl/IdentifyDaoImpl.java
@@ -9,20 +9,12 @@ import in.gov.nvli.harvester.dao.IdentifyDao;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-
 /**
  *
  * @author vootla
  */
 @Repository
 @Transactional(readOnly = true)
-public class IdentifyDaoImpl implements IdentifyDao{
+public class IdentifyDaoImpl implements IdentifyDao {
 
-    @Override
-    public void saveREpositoryInformation() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    
-    
 }
