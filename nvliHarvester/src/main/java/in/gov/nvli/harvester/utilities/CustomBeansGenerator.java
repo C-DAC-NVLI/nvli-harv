23a8bb75b6e65495643226a936c49c1f4aa05991
vootla <vootla@hcdc-pc22.pune.cdac.in>
2016-04-27 13:50:16 +0530

    update:added required fields

--- nvliHarvester/src/main/java/in/gov/nvli/harvester/utilities/CustomBeansGenerator.java
+++ nvliHarvester/src/main/java/in/gov/nvli/harvester/utilities/CustomBeansGenerator.java
@@ -6,6 +6,8 @@
 package in.gov.nvli.harvester.utilities;
 
 import in.gov.nvli.harvester.beans.HarRepo;
+import in.gov.nvli.harvester.beans.HarRepoStatus;
+import in.gov.nvli.harvester.beans.HarRepoType;
 import in.gov.nvli.harvester.customised.HarRepoCustomised;
 
 /**
@@ -16,20 +18,53 @@ public class CustomBeansGenerator {
   
     public static HarRepoCustomised convertHarRepoToHarRepoCustomised(HarRepo harRepo)
     {
-        HarRepoCustomised custObj=new HarRepoCustomised();
-        custObj.setRepoId(harRepo.getRepoId());
-        custObj.setRepoBaseUrl(harRepo.getRepoBaseUrl());
-        custObj.setRepoName(harRepo.getRepoName());
-        custObj.setRepoLastSyncDate(harRepo.getRepoLastSyncDate());
-        return  custObj;
+        HarRepoCustomised repositoryObject=new HarRepoCustomised();
+       repositoryObject.setRepoName(harRepo.getRepoName());
+            repositoryObject.setRepoBaseUrl(harRepo.getRepoBaseUrl());
+            repositoryObject.setRepoProtocolVersion(harRepo.getRepoProtocolVersion());
+            repositoryObject.setRepoEarliestTimestamp(harRepo.getRepoEarliestTimestamp());
+            repositoryObject.setRepoGranularityDate(harRepo.getRepoGranularityDate());
+            repositoryObject.setRepoDeletionMode(harRepo.getRepoDeletionMode());
+            repositoryObject.setRepoEmail(harRepo.getRepoEmail());
+            repositoryObject.setRepoDesc(harRepo.getRepoDesc());
+            repositoryObject.setRepoCompression(harRepo.getRepoCompression());
+            repositoryObject.setRepoRegistrationDate(harRepo.getRepoRegistrationDate());
+            repositoryObject.setRepoLink(harRepo.getRepoLink());
+            repositoryObject.setRepoTypeId(harRepo.getRepoTypeId().getRepoTypeId());
+            repositoryObject.setRepoSiteUrl(harRepo.getRepoSiteUrl());
+            repositoryObject.setRepoPermanentLink(harRepo.getRepoPermanentLink());
+            repositoryObject.setRepoLatitude(harRepo.getRepoLatitude());
+            repositoryObject.setRepoLongitude(harRepo.getRepoLongitude());
+            repositoryObject.setRepoStatusId(harRepo.getRepoStatusId().getRepoStatusId());
+            repositoryObject.setRepoLastSyncDate(harRepo.getRepoLastSyncDate());
+            repositoryObject.setRepoActivationDate(harRepo.getRepoActivationDate());
+       
+        return  repositoryObject;
     }
      public static HarRepo convertHarRepoCustomisedToHarRepo(HarRepoCustomised harRepoCustomised)
     {
-        HarRepo harRepoObj=new HarRepo();
-        harRepoObj.setRepoId(harRepoCustomised.getRepoId());
-        harRepoObj.setRepoBaseUrl(harRepoCustomised.getRepoBaseUrl());
-        harRepoObj.setRepoName(harRepoCustomised.getRepoName());
-        harRepoObj.setRepoLastSyncDate(harRepoCustomised.getRepoLastSyncDate());
-        return  harRepoObj;
+        HarRepo repositoryObject=new HarRepo();
+        
+            repositoryObject.setRepoName(harRepoCustomised.getRepoName());
+            repositoryObject.setRepoBaseUrl(harRepoCustomised.getRepoBaseUrl());
+            repositoryObject.setRepoProtocolVersion(harRepoCustomised.getRepoProtocolVersion());
+            repositoryObject.setRepoEarliestTimestamp(harRepoCustomised.getRepoEarliestTimestamp());
+            repositoryObject.setRepoGranularityDate(harRepoCustomised.getRepoGranularityDate());
+            repositoryObject.setRepoDeletionMode(harRepoCustomised.getRepoDeletionMode());
+            repositoryObject.setRepoEmail(harRepoCustomised.getRepoEmail());
+            repositoryObject.setRepoDesc(harRepoCustomised.getRepoDesc());
+            repositoryObject.setRepoCompression(harRepoCustomised.getRepoCompression());
+            repositoryObject.setRepoRegistrationDate(harRepoCustomised.getRepoRegistrationDate());
+            repositoryObject.setRepoLink(harRepoCustomised.getRepoLink());
+            repositoryObject.setRepoTypeId(new HarRepoType(harRepoCustomised.getRepoTypeId()));
+            repositoryObject.setRepoSiteUrl(harRepoCustomised.getRepoSiteUrl());
+            repositoryObject.setRepoPermanentLink(harRepoCustomised.getRepoPermanentLink());
+            repositoryObject.setRepoLatitude(harRepoCustomised.getRepoLatitude());
+            repositoryObject.setRepoLongitude(harRepoCustomised.getRepoLongitude());
+            repositoryObject.setRepoStatusId(new HarRepoStatus(harRepoCustomised.getRepoStatusId()));
+            repositoryObject.setRepoLastSyncDate(harRepoCustomised.getRepoLastSyncDate());
+            repositoryObject.setRepoActivationDate(harRepoCustomised.getRepoActivationDate());
+       
+        return  repositoryObject;
     }
 }
