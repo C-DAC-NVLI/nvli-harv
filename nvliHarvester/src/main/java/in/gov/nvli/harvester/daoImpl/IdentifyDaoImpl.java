1c63c2f2e78210345ad54a3d8ceed5e0029f3f31
Ankit Sharma <sankit@cdac.in>
2016-04-27 18:33:20 +0530

    code optimization and formatting

--- nvliHarvester/src/main/java/in/gov/nvli/harvester/daoImpl/IdentifyDaoImpl.java
+++ nvliHarvester/src/main/java/in/gov/nvli/harvester/daoImpl/IdentifyDaoImpl.java
@@ -6,6 +6,8 @@
 package in.gov.nvli.harvester.daoImpl;
 
 import in.gov.nvli.harvester.dao.IdentifyDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,4 +19,5 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class IdentifyDaoImpl implements IdentifyDao {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(IdentifyDaoImpl.class);
 }
