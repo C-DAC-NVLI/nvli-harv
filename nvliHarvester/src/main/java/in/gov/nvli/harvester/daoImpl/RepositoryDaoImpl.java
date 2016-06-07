72e1269d48e6eb5510957ad19a247e496c58678f
vootla <vootla@hcdc-pc22.pune.cdac.in>
2016-06-07 18:53:51 +0530

    update: external jars location

--- nvliHarvester/src/main/java/in/gov/nvli/harvester/daoImpl/RepositoryDaoImpl.java
+++ nvliHarvester/src/main/java/in/gov/nvli/harvester/daoImpl/RepositoryDaoImpl.java
@@ -41,7 +41,7 @@ public class RepositoryDaoImpl extends GenericDaoImpl<HarRepo, Integer> implemen
     @TransactionalReadOrWrite
     public HarRepo addRepository(HarRepo repositoryObject) {
         try {
-            if(getRepositoryByUID(repositoryObject.getRepoUID()) == null){
+            if(getRepositoryByUID(repositoryObject.getRepoUID()) == null && getRepository(repositoryObject.getRepoBaseUrl())==null){
                 createNew(repositoryObject);
             }else{
                 LOGGER.error("Repository with UID --> "+repositoryObject.getRepoUID() +" is already available");
